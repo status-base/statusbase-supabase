e1a4900e4620134accfa56e5266ad1e83a239f80
zernonia <zernonia@gmail.com>
2022-06-16 23:12:28 +0800

    update to use service role key

--- supabase/utils/client.ts
+++ supabase/utils/client.ts
@@ -4,5 +4,5 @@ export const supabaseClient = createClient(
   // Supabase API URL - env var exported by default when deployed.
   Deno.env.get("SUPABASE_URL") ?? "",
   // Supabase API ANON KEY - env var exported by default when deployed.
-  Deno.env.get("SUPABASE_ANON_KEY") ?? ""
+  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? ""
 )
